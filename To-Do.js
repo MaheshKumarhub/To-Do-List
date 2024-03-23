1319bcb18a2090c2e565e1335dd210f5ed39fbd6
MaheshKumarhub <mk708133@gmail.com>
2024-03-23 12:16:32 +0530

    favicon

--- To-Do.js
+++ To-Do.js
@@ -20,7 +20,7 @@ function addTask(){
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
-        saveData();
+        saveData();  
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
